36f7b5adb9190a95cd3e1c83cac73ff53889f272
Gavin Wilson <gmanwilson@icloud.com>
2025-04-09 13:32:15 -0400

    updated git ignore

--- app.py
+++ app.py
@@ -3,9 +3,28 @@ import requests
 
 app = Flask(__name__)
 
+# Hardcoded data to test API endpoints
+local_weather = {
+    'florida' : {
+        'tampa' : {
+            'temperature' : 70,
+            'humidity' : '40%',
+            'cloud' : None
+        }
+    }
+}
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return '<h1>Let\'s find the weather! </h1>'
+
+@app.route('/weather')
+def get_weather():
+    tampa = local_weather.get('florida').get('tampa')
+    
+    tampa_weather = f'In tampa it is {tampa.get('temperature')} degrees fahrenheit, with {tampa.get('humidity')} humidity and {'some clouds' if tampa.get('cloud') else 'no clouds'}'
+
+    return tampa_weather
 
 if __name__ == '__main__':
     app.run()
