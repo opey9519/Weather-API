547c6c3635378875b4fc82ccd0f1cd2ac52e75a1
Gavin Wilson <gmanwilson@icloud.com>
2025-04-11 16:21:08 -0400

    Refined backend and added frontend HTML/CSS

--- app.py
+++ app.py
@@ -1,33 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import requests
 from dotenv import load_dotenv
 import os
 
-load_dotenv() # Brings all environment variables from .env into os.environ
+load_dotenv()  # Brings all environment variables from .env into os.environ
 app = Flask(__name__)
 
 API_KEY = os.getenv('WEATHER_API')
+
 
 @app.route('/')
 def home():
     return '<h1>Let\'s find the weather! </h1>'
 
-@app.route('/weather')
-
-# Get weather data for specified location
+# Get weather data for specified location using query parameters
 @app.route('/weather/<location>', methods=['GET'])
 def get_weather(location):
+
     URL = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?key={API_KEY}'
 
     try:
         response = requests.get(URL)
         if response.status_code == 200:
             data = response.json()
-            print()
-        return f'{data.get('days')[0].get('temp')}', 200
-    
+            return jsonify({'temp': data.get('days')[0].get('temp')}), 200
+        else:
+            return jsonify({'error' : 'could not fetch data'}), response.status_code
+
     except requests.exceptions.RequestException as e:
-        return f"Error: {e}", 500
+        return jsonify({'error' : f"Error: {e}"}), 500
+
 
 if __name__ == '__main__':
     app.run()
